4012ba9e3a41c4b83857d54fbb96cd8acb477605
Mario Portillo <marioportillo@Marios-MacBook-Pro.local>
2019-12-10 20:31:28 -0600

    Completed part 2 of the Angular Lesson 1 Exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,6 +10,7 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://www.servedfromscratch.com/wp-content/uploads/2018/10/IMG_2414.jpg';
   image2 = 'https://data2.thefeedfeed.com/static/2019/02/25/15511105485c741194c56ba.jpg';
   image3 = 'https://www.101dogbreeds.com/wp-content/uploads/2015/07/Jack-Chi.jpg';
+  image4 = 'https://media1.giphy.com/media/zOvBKUUEERdNm/giphy.gif';
 
   constructor() { }
 
