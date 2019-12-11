2e23bff354bd2267f5b4f6a82f3ea1829280fa50
Mario Portillo <marioportillo@Marios-MacBook-Pro.local>
2019-12-10 19:53:32 -0600

    Completed part 1 of Angular Lesson 1 Exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://www.servedfromscratch.com/wp-content/uploads/2018/10/IMG_2414.jpg';
+  image2 = 'https://data2.thefeedfeed.com/static/2019/02/25/15511105485c741194c56ba.jpg';
+  image3 = 'https://www.101dogbreeds.com/wp-content/uploads/2015/07/Jack-Chi.jpg';
 
   constructor() { }
 
